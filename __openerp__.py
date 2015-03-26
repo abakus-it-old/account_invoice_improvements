9e1772ff3ac6f0e9ade20a19f8f514b6dd27da97
Bernard Delhez <bernarddelhez@gmail.com>
2015-03-26 09:39:23 +0100

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -8,6 +8,7 @@
     'description': """This modules adds some functionalities to the invoicing process for AbAKUS. 
 - it adds a field in the invoice form with the next invoice number
 - it auto copies the supplier ref number to the bank transfer communication field
+- it checks if the supplier invoice number already exists
 
 This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.""",
     'data': ['account_invoice_view.xml',],
