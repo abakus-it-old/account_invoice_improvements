4017e7bb744dffac36daa553a5da1d4bc1275aa0
Bernard Delhez <bernarddelhez@gmail.com>
2015-04-14 17:27:34 +0200

    add account_move_line_view.xml + update description

--- __openerp__.py
+++ __openerp__.py
@@ -5,12 +5,16 @@
     'author': "Valentin THIRION, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
     'category': 'Accounting',
-    'description': """This modules adds some functionalities to the invoicing process for AbAKUS. 
-- it adds a field in the invoice form with the next invoice number
-- it auto copies the supplier ref number to the bank transfer communication field
-- it checks if the supplier invoice number already exists
-
-This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.""",
-    'data': ['account_invoice_view.xml',],
+    'description': 
+    """
+    This modules adds some functionalities to the invoicing process for AbAKUS. 
+        - it adds a field in the invoice form with the next invoice number
+        - it auto copies the supplier ref number to the bank transfer communication field
+        - it checks if the supplier invoice number already exists
+        - it adds a group by in account.move.line that groups by journal entry
+    
+    This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
+    """,
+    'data': ['account_invoice_view.xml','account_move_line_view.xml'],
     'demo': [],
 }
