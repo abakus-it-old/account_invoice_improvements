3472961523fa117cbf18a5d660344a607ac5574a
Bernard Delhez <bernarddelhez@gmail.com>
2015-04-21 11:53:28 +0200

    New filter and treeview fields order

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 This modules adds some functionalities to the invoicing process for AbAKUS.
 ---------------------------------------------------------------------------
-  - it adds a field in the invoice form with the next invoice number
-  - it auto copies the supplier ref number to the bank transfer communication field
-  - it checks if the supplier invoice number already exists
-  - it adds a group by in account.move.line that groups by journal entry
+    - it adds a field in the invoice form with the next invoice number.
+    - it auto copies the supplier ref number to the bank transfer communication field.
+    - it checks if the supplier invoice number already exists.
+    - it adds a group by in account.move.line that groups by journal entry.
+    - it changes the fields order in the treeview of account.move.line.
+    - it adds a filter that filters on special journals and accounts
 
 This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
--- __openerp__.py
+++ __openerp__.py
@@ -8,13 +8,15 @@
     'description': 
     """
     This modules adds some functionalities to the invoicing process for AbAKUS. 
-        - it adds a field in the invoice form with the next invoice number
-        - it auto copies the supplier ref number to the bank transfer communication field
-        - it checks if the supplier invoice number already exists
-        - it adds a group by in account.move.line that groups by journal entry
+        - it adds a field in the invoice form with the next invoice number.
+        - it auto copies the supplier ref number to the bank transfer communication field.
+        - it checks if the supplier invoice number already exists.
+        - it adds a group by in account.move.line that groups by journal entry.
+        - it changes the fields order in the treeview of account.move.line.
+        - it adds a filter that filters on special journals and accounts
     
     This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions, under the control of Valentin Thirion.
     """,
-    'data': ['account_invoice_view.xml','account_move_line_view.xml'],
+    'data': ['view/account_invoice_view.xml','view/account_move_line_view.xml'],
     'demo': [],
 }
